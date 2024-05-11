06f4ba0b9ece5da6280b7d95a0ef2fc2246a9d74
Petr Haton <petr.haton@unicov-net.com>
2024-05-12 01:21:00 +0200

    refactor: Update to kubebuilder and new version

--- internal/controller/nfs_controller.go
+++ internal/controller/nfs_controller.go
@@ -153,7 +153,7 @@ func (r *NfsReconciler) pvcForNfs(m *storagev1.Nfs) *v1.PersistentVolumeClaim {
 		},
 		Spec: v1.PersistentVolumeClaimSpec{
 			AccessModes: []v1.PersistentVolumeAccessMode{v1.ReadWriteMany},
-			Resources: v1.ResourceRequirements{
+			Resources: v1.VolumeResourceRequirements{
 				Requests: v1.ResourceList{
 					v1.ResourceName(v1.ResourceStorage): resource.MustParse("1Gi"),
 				},
