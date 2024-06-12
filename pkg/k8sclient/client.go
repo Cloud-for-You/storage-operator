c4ea9ccb7b9c001e551a5dfd63de28d6462a8f5d
Petr Haton <petr.haton@unicov-net.com>
2024-06-12 22:50:31 +0200

    Pridani lokalniho k8s klienta, ktery je mozne vyuzivat v dalsich volanich

---
+++ pkg/k8sclient/client.go
@@ -0,0 +1,34 @@
+package k8sclient
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func K8SClient() (*kubernetes.Clientset, error) {
+	var config *rest.Config
+	var err error
+
+	config, err = rest.InClusterConfig()
+	if err != nil {
+		// If not in cluster, use the kubeconfig file from the user's home directory
+		kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to build kubeconfig: %v", err)
+		}
+	}
+
+	// Create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kubernetes clientset: %v", err)
+	}
+
+	return clientset, nil
+}
